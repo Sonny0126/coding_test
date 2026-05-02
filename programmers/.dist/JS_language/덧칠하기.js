3640a8166b658130a166804a44194a4a087ae791
Son Seung Min <sonsmmm@naver.com>
2026-05-02 23:20:39 +0900

    Programmers C_language start

--- programmers/.dist/JS_language/덧칠하기.js
+++ programmers/.dist/JS_language/덧칠하기.js
@@ -3,15 +3,15 @@
 //section은 총 칠해야 할 부분
 //최소 사용 횟수 구하기
 function solution(n, m, sections) {
-  let ans = 0;
+  let cnt = 0;
   let painted = 0; //현재까지 롤러롤 칠한 마지막 위치
   for (let section of sections) {
     //section은 이번에 칠해야 하는 위치
     if (painted < section) {
       //아직 안 칠해졌으면 롤러를 새로 시작
-      ans++;
+      cnt++;
       painted = section + m - 1;
     }
   }
-  return ans;
+  return cnt;
 }
