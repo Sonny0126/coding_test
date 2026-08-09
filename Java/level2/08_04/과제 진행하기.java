d71bf8d243f05dd5124e81b7f67930285e87572b
Son Seung Min <sonsmmm@naver.com>
2026-08-09 18:42:56 +0900

    과제 진행하기(수정)

--- Java/level2/08_04/과제 진행하기.java
+++ Java/level2/08_04/과제 진행하기.java
@@ -11,40 +11,50 @@ class Solution {
         Stack<String[]> stack = new Stack<>();
         int idx = 0;
 
+        //현재 진행중인 과제
         String[] cur = plans[0];
-
         for (int i = 1; i < plans.length; i++) {
             String[] next = plans[i];
-
+            //현재 과제의 종료시간
             int curEnd = time(cur[1]) + Integer.parseInt(cur[2]);
+            //다음 과제의 시작 시간
             int nextStart = time(next[1]);
 
+            //현재 과제가 다음과제 시작전에 끝나는 경우
             if (curEnd <= nextStart) {
+              //현재 과제 완료
                 answer[idx++] = cur[0];
 
                 int now = curEnd;
 
+                //남은 시간동안 멈춰둔 과제 처리
                 while (!stack.isEmpty()) {
+                  //가장 최근에 멈춘과제부터 꺼냄
                     String[] stopped = stack.pop();
                     int remain = Integer.parseInt(stopped[2]);
 
+                    //멈춘 과제까지 완료할 수 있는 경우
                     if (now + remain <= nextStart) {
                         now += remain;
                         answer[idx++] = stopped[0];
-                    } else {
+                    } 
+                    else {
+                      //시간 부족하면 남는시간만 저장ㅇ하고 다시 멈춤
                         stopped[2] = String.valueOf(remain - (nextStart - now));
                         stack.push(stopped);
                         break;
                     }
                 }
-            } else {
+            } 
+            else {
+              //다음 과제 먼저 시작하므로 현재 과제 종료
                 cur[2] = String.valueOf(curEnd - nextStart);
                 stack.push(cur);
             }
-
+            //다음 과제 현재 과제로 변경
             cur = next;
         }
-
+        //마지막 과제 완료
         answer[idx++] = cur[0];
 
         while (!stack.isEmpty()) {
@@ -54,7 +64,7 @@ class Solution {
         return answer;
     }
 
-    private int time(String s) {
+    public int time(String s) {
         String[] t = s.split(":");
         return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
     }
