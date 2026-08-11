0ab8dca456044cba518730a1124b7e314cf46ccc
Son Seung Min <sonsmmm@naver.com>
2026-08-11 13:30:52 +0900

    N-Queen

--- Java/level2/08_06/N-Queeen.java
+++ Java/level2/08_06/N-Queeen.java
@@ -5,10 +5,13 @@ class Solution {
         //가로 세로 길이 n
         //n개의 퀸 서로 공격 못하게 가로, 세로, 대각선 X
         //n개의 퀸이 만족하도록 배치하는 방법의 수 return
+        
+        //1개일 때 가능
+        if(n==1) return 1;
+        
+        if(n<4 && n!=1) return answer;
 
-        if(n<4) return answer;
-
-        int [] q = new int[n]; //배열로 퀸 위치 기록
+        int [] q = new int[n];
         answer = dfs(0, n, q);
 
         return answer;
@@ -23,18 +26,15 @@ class Solution {
         int count =0;
 
         for(int col = 0; col<n; col++){
-          if(check(row, col, queen)){
-            //안전하다면 위치를 기록하고
+          if(isPossible(row, col, queen)){
             queen[row] = col;
-            //dfs를 호출 (횟수는 count++)
             count += dfs(row+1, n, queen);
           }
         }
       return count;
     }
 
-    //퀸을 놓을 수 있는지 확인하는 함수
-    public boolean check(int row, int col, int[] queen){
+    public boolean isPossible(int row, int col, int[] queen){
       for(int x=0; x<row; x++){
         int y = queen[x];
 
