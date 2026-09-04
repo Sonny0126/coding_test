919f7dad10ef8736a07d6d19b0f7f84e3948727f
Son Seung Min <sonsmmm@naver.com>
2026-09-04 12:04:47 +0900

    핸드폰 번호가리기

--- Java/9월/09_04/핸드폰 번호 가리기.java
+++ Java/9월/09_04/핸드폰 번호 가리기.java
@@ -1,8 +1,10 @@
 class Solution {
     public String solution(String phone_number) {
         
-        
+        String str = phone_number.substring(0, phone_number.length()-4);
 
-        return answer;
+        String star = "*".repeat(str.length());
+
+        return phone_number.replace(str, star);
     }
 }
