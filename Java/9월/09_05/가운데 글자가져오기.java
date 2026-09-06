c71ce59ba4cece334d9d947bc155669a362b8c64
Son Seung Min <sonsmmm@naver.com>
2026-09-06 17:50:42 +0900

    가운데 글자 가져오기

---
+++ Java/9월/09_05/가운데 글자가져오기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String s) {
+        int mid = s.length() / 2;
+
+        if (s.length() % 2 == 1) {
+            return s.substring(mid, mid + 1);
+        } else {
+            return s.substring(mid - 1, mid + 1);
+        }
+    }
+}
