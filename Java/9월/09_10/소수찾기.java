ddba42980d69036807d485762b825fd35c0710b1
Son Seung Min <sonsmmm@naver.com>
2026-09-10 13:03:56 +0900

    소수찾기

--- Java/9월/09_10/소수찾기.java
+++ Java/9월/09_10/소수찾기.java
@@ -11,7 +11,7 @@ class Solution {
     }
 
     boolean check(int n) {
-        for(int i=2; i<n; i++) {
+        for(int i=2; i*i<=n; i++) {
             if(n % i == 0) {
                 return false;
             }
